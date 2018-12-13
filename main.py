a6ba2e8be35eed684d18250d11e1d431cfb5cba8
Maya Khalifé <mayakhalife@Mayas-MacBook-Pro.local>
2018-12-13 20:38:07 +0100

    Worked on new RIS excel sheet format, and continued working on sorting rooms (that i just realized is useless)

--- main.py
+++ main.py
@@ -29,10 +29,10 @@ def sort_indications(df1):
     plt.ylabel('Number of occurrences')
     plt.xticks(range(len(indic)))
     plt.show()
-    fig.savefig('/Users/mayakhalife/Documents/CHV_RIS/MSKindications.svg')   # save the figure to file
+    fig.savefig('/Users/mayakhalife/Documents/CHV_RIS/MSK/MSKindications.svg')   # save the figure to file
     df2 = pd.DataFrame({'Indications': indic})
     # Create a Pandas Excel writer using XlsxWriter as the engine.
-    writer = pd.ExcelWriter('/Users/mayakhalife/Documents/CHV_RIS/MSKindications.xlsx', engine='xlsxwriter')
+    writer = pd.ExcelWriter('/Users/mayakhalife/Documents/CHV_RIS/MSK/MSKindications.xlsx', engine='xlsxwriter')
     # Convert the dataframe to an XlsxWriter Excel object.
     df2.to_excel(writer, sheet_name='Sheet1')
     # Close the Pandas Excel writer and output the Excel file.
@@ -50,27 +50,30 @@ def sort_patient_age(df1):
     plt.ylabel('Number of occurrences')
     plt.show()
 
-def sort_patient_age(df1):
+def sort_rooms(df1):
     # get Xray room/scanner
-    rooms = np.unique(df1.Salles).tolist()
+    for i in range(len(df1.columns)):
+        if df1.columns[i] == "Salle(s)":
+            c_rooms = df1[df1.columns[i]].values
+    rooms = np.unique(c_rooms.tolist())
     nroom = np.zeros(len(rooms))
     # count number of rooms  occurrence
-    for i in range(len(df1.Salles)):
+    for i in range(len(c_rooms)):
         for j in rooms:
-            if df1.Salles[i] == j:
-                nroom[rooms.index(j)] = nroom[rooms.index(j)]+1
-    print(nroom)
+            if c_rooms[i] == j:
+                nroom[rooms[j]] = nroom[rooms[j]]+1
+
 
 def main():
     """Main"""
     # Assign spreadsheet filename to `file`
-    filename_ = '/Users/mayakhalife/Documents/CHV_RIS/radiojan-juin18.xlsx'
+    filename_ = '/Users/mayakhalife/Documents/CHV_RIS/MSK/msk_toutes_salle_radio_2017-2018.xlsx'
 
     # Load spreadsheet
     df1 = pd.read_excel(filename_, dtype={'Name': str, 'Value': str}, skiprows=[0, 1, 2])
     # sort_indications(df1)
     # sort_patient_age(df1)
-    sort_patient_age(df1)
+    sort_rooms(df1)
 
 if __name__ == '__main__':
     main()
