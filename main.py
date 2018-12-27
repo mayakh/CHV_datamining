efa5b033dd9378659cb6b1234b4943611a06dd6f
Maya Khalifé <maya.khalife@arterys.com>
2018-12-28 01:20:37 +0200

    Counting anatomy cases in case of ER radiology based on word lookup

--- main.py
+++ main.py
@@ -18,14 +18,11 @@ def sort_patient_age(df1):
     age_list = list(df1['Age'])
     age = np.zeros(len(age_list))
     for j in range(len(age_list)):
-        print(str(age_list[j]))
         if "an" in age_list[j]:
             a_index = str(age_list[j]).index('a')
             age[j] = age_list[j][0:a_index-1]
-            print(int(age[j]))
         else:
             age[j] = 0
-            print(age[j])
 
     # birthdate = np.zeros((len(df1[df1.columns[5]].values), 1))
     nind = np.zeros([9])
@@ -57,16 +54,55 @@ def sort_indications(df1):
     #df1 = xl.parse('radio jan-juin18 - Copie')g
     indic = list(df1['Examen(s)'])
     indic_u = list(np.unique(indic))
-    nind = np.zeros(len(indic_u))
+    nind = np.zeros([20])
+    # create a shortlist
+    dpt = list(df1['UF   Demandeur'])
     # count number of indicated exam occurrence
     for i in range(len(indic)):
-        if 'Genou 1 ou 2 incidences' in indic[i]:
-            nind[0] = nind[0]+1
-            print(indic[i])
-
+        if dpt[i] == '0991 URGENCES':
+            if 'Genou' in indic[i]:
+                nind[0] = nind[0]+1
+            elif 'Coude' in indic[i]:
+                nind[1] = nind[1]+1
+            elif 'Main' in indic[i]:
+                nind[2] = nind[2] + 1
+            elif 'Poignet' in indic[i]:
+                nind[3] = nind[3] + 1
+            elif 'Hanche' in indic[i]:
+                nind[4] = nind[4] + 1
+            elif 'Pied' in indic[i]:
+                nind[5] = nind[5] + 1
+            elif 'Jambe' in indic[i]:
+                nind[6] = nind[6] + 1
+            elif 'Calcaneum' in indic[i]:
+                nind[7] = nind[7] + 1
+            elif 'Cheville' in indic[i]:
+                nind[8] = nind[8] + 1
+            elif 'Femur' in indic[i]:
+                nind[9] = nind[9] + 1
+            elif 'Rachis' in indic[i]:
+                nind[10] = nind[10] + 1
+            elif 'Humerus' in indic[i]:
+                nind[11] = nind[11] + 1
+            elif 'Bassin' in indic[i]:
+                nind[12] = nind[12] + 1
+            elif 'membre inf' in indic[i]:
+                nind[13] = nind[13] + 1
+            elif 'membre sup' in indic[i]:
+                nind[14] = nind[14] + 1
+            elif 'Articulation' in indic[i]:
+                nind[15] = nind[15] + 1
+            elif 'Avant-bras' in indic[i]:
+                nind[16] = nind[16] + 1
+            elif 'scapulaire' in indic[i]:
+                nind[17] = nind[17] + 1
+            elif 'Opn' in indic[i]:
+                nind[18] = nind[18] + 1
+            elif 'Sternum' in indic[i]:
+                nind[19] = nind[19] + 1
     print(nind)
     print(indic_u)
-    print(len(indic_u))
+    # print(len(indic_u))
     # fig, ax = plt.subplots()
     # plt.bar(range(len(indic)), nind)
     # plt.ylabel('Number of occurrences')
